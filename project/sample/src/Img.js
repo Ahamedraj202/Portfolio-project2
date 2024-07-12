7e6dcc6a01291a1bc0bb78db0bb0b47ded8f1b8b
Ahamedraj202 <Rajelectro00@gmail.com>
2024-07-12 16:16:26 +0530

    Portfolio project2 completed

---
+++ project/sample/src/Img.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Img = () => {
+  return (
+    <div className='img'>
+        <img className='image' src='c:\Users\cselab\Downloads\WhatsApp Image 2024-07-12 at 4.09.14 PM.jpeg'/>
+    </div>
+  )
+}
+
+export default Img;
